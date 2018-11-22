8597097da356dd81d92ca8ecf15b4fc58bec945a
anindith123 <anindith123@ymail.com>
2018-11-22 03:59:25 -0500

    another test django

---
+++ projects/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Project
+from .serializers import ProjectSerializer
+
+
+
+
+# Create your views here.
+def home(request):
+    if request.method == "GET":
+        project_list = Project.objects
+        serializer = ProjectSerializer(project_list, many = True)
+        return JsonResponse(serializer.data, safe=False)
+
+# Create your views here.
